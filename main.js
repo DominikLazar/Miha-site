6d67ef10fc54b7ea00360197fa320a01aca89c76
Dominik Lazar <47496013+DominikLazar@users.noreply.github.com>
2022-06-28 15:08:59 +0200

    navbar pokusaji

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".primary-navigation");
+
+var sideNav = document.getElementById("primary-navigation");
+var menuBtn = document.getElementById("hamburger");
+
+menuBtn.onclick = function () {
+  if (hamburger.classList == "is-active") {
+    sideNav.style.transform = "translateX(0%)";
+    console.log("klik");
+  } else {
+    sideNav.style.transform = "translateX(100%)";
+    console.log("klik opet");
+  }
+};
 
 hamburger.addEventListener("click", function () {
   this.classList.toggle("is-active");
+  if (hamburger.classList == "hamburger is-active")
+    sideNav.style.right = "-250px";
+  else sideNav.style.right = "0px";
 });
