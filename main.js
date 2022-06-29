eedbe795057699d97d9f5586247b8497414af417
Dominik Lazar <47496013+DominikLazar@users.noreply.github.com>
2022-06-29 14:19:25 +0200

    navbar working

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
 const hamburger = document.querySelector(".hamburger");
-const navbar = document.querySelector(".primary-navigation");
+const navbar = document.querySelector("primary-navigation");
 
 var sideNav = document.getElementById("primary-navigation");
 var menuBtn = document.getElementById("hamburger");
 
-menuBtn.onclick = function () {
-  if (hamburger.classList == "is-active") {
+hamburger.addEventListener("click", function () {
+  this.classList.toggle("is-active");
+  const visibility = sideNav.getAttribute("data-visible");
+
+  console.log(visibility);
+  if (visibility === "false") {
+    sideNav.setAttribute("data-visible", true);
+    hamburger.setAttribute("aria-expanded", true);
     sideNav.style.transform = "translateX(0%)";
+  } else if (visibility === "true") {
+    sideNav.setAttribute("data-visible", false);
+    hamburger.setAttribute("aria-expanded", false);
+    sideNav.style.transform = "translateX(100%)";
+  }
+});
+/*
+menuBtn.onclick = function () {
+  if (hamburger.classList == "hamburger is-active") {
     console.log("klik");
   } else {
-    sideNav.style.transform = "translateX(100%)";
     console.log("klik opet");
   }
 };
-
-hamburger.addEventListener("click", function () {
-  this.classList.toggle("is-active");
   if (hamburger.classList == "hamburger is-active")
     sideNav.style.right = "-250px";
   else sideNav.style.right = "0px";
+
+hamburger.addEventListener("click", () => {
+  const visibility = sideNav.getAttribute("data-visible");
+  console.log(visibility);
 });
+*/
